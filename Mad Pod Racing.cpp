26763c8ba0d6649f9d7871c65fc6efd312ffa780
95gas <46025764+95gas@users.noreply.github.com>
2023-04-16 16:04:56 +0200

    Update Mad Pod Racing.cpp
    Add boost manager and classes

--- Mad Pod Racing.cpp
+++ Mad Pod Racing.cpp
@@ -6,50 +6,126 @@
 
 using namespace std;
 
+
+class Checkpoint {       // The class
+  public:             // Access specifier
+    int x;        // Attribute (int variable)
+    int y;  // Attribute (string variable)
+};
+
 int distanceBetweenTwoPoints(int x, int y, int a, int b){
-return sqrt(pow(x - a, 2) + pow(y - b, 2));
+    return sqrt(pow(x - a, 2) + pow(y - b, 2));
 }
 
-int calculate_thrust(int x, int y, int opponent_x, int opponent_y,int* boost, int next_checkpoint_angle, int next_checkpoint_x, int next_checkpoint_y, int next_checkpoint_distance, int next_prev_checkpoint_distance,int CheckpointRadius){
-    int thrust = 100;
-    int dist = distanceBetweenTwoPoints(x, y,next_checkpoint_x, next_checkpoint_y);
+
+int calculate_thrust(std::vector< Checkpoint > & list, int lap, int x, int y, int prev_x, int prev_y, int next_checkpoint_angle, int next_checkpoint_x, int next_checkpoint_y, int next_checkpoint_distance, int next_prev_checkpoint_distance,int CheckpointRadius){
+    double thrust = 100;
+
+    if (abs(next_checkpoint_angle) > 18){
+        thrust*= max(min((1 - (abs(next_checkpoint_angle)/90.0)), 1.0), 0.0);
+        thrust*= max(min((next_checkpoint_distance/(double)(CheckpointRadius*2)), 1.0), 0.0);
+    }
+
+    // check drift
+    int speed = sqrt((x - prev_x)*(x - prev_x) + (y - prev_y)*(y - prev_y));
+
+    if ( speed*3 > next_checkpoint_distance and lap > 0 and -6 < next_checkpoint_angle < 6){
+
+        int index_next = 0;
+
+        for(unsigned int i = 0; i < list.size(); i++){
+
+                    if (list[i].x == next_checkpoint_x and next_checkpoint_y == list[i].y){
+
+                        if (i!=list.size()-1){
+                            index_next = i++;
+                        }
+            
+                    }
+        }
+
+        cout << list[index_next].x << " " << list[index_next].y << " " << 40 << endl;
+        
+        return -1;           
+        }
+
+                
+
+    return int(thrust);
+}
+
+
+Checkpoint CheckpointManager(std::vector< Checkpoint > & list, std::vector< int > & dist, Checkpoint nextCheckpoint, Checkpoint firstCheckpoint, int next_checkpoint_dist, int& lap){
     
-    if (abs(next_checkpoint_angle) < 10){
+    Checkpoint best_check;
+    bool newLap = false;
 
-        if (*boost == 0){ // is the boost available?
+    cerr << "Run function\n" << endl;
 
-        // are we heading towards a checkpoint?
-            if (next_checkpoint_distance>=4500){
+    if (lap == 0){
 
-                *boost = 1;
-                return 101; // BOOST
+        if (list[list.size()-1].x != nextCheckpoint.x and nextCheckpoint.y != list[list.size()-1].y){
+        
+            if (nextCheckpoint.x != firstCheckpoint.x and firstCheckpoint.y != nextCheckpoint.y){
+                list.push_back(nextCheckpoint);
+                dist.push_back(next_checkpoint_dist);
             }
-         
+            else{
+                newLap = true;
+                lap = lap+1;
+                cerr << "NewLap\n" << endl;
+            }
+        
         }
     }
-    else{
-    
-        thrust*= max(min((1 - abs(next_checkpoint_angle)/90), 1), 0);
-        thrust*= max(min((next_checkpoint_distance/(CheckpointRadius*2)), 1), 0);
-    }
+
+    if (lap==1){
+
+        // find best checkpoint where to boost
+
+        // compute distance
+
+        // find best spot where to use BOOST
+
+        int index_max = 0;
+        int max=0;
         
-    cerr << "Debug messages..." << endl;
-    cerr << next_checkpoint_distance << endl;
+         for(unsigned int i = 0; i < dist.size(); i++){
+
+            if (dist[i]>max){
+                index_max = i;
+                max = dist[i];
+            }
+        }
+
+        best_check = list[index_max];
+
+        return best_check;
+    }
     
-    return thrust;
+    best_check.x=-1;
+    best_check.y=-1;
+
+    return best_check;
 }
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 
 int main()
 {
 
-    int BOOST = 0;
-    
+    bool boost = true;
 
+    std::vector< Checkpoint > list;
+    std::vector< int > dist;
+
+    bool game_started = false;
+    Checkpoint first_checkpoint;
+
+    int lap = 0;
+
+    int prev_x= 0;
+    int prev_y = 0;
+    
     int next_prev_checkpoint_dist = 0;
 
     // game loop
@@ -65,22 +141,51 @@ int main()
         int opponent_y;
         cin >> opponent_x >> opponent_y; cin.ignore();
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+        // save first checkpoint
+        if (!game_started){
+            first_checkpoint.x=next_checkpoint_x;
+            first_checkpoint.y=next_checkpoint_y;
+            game_started = true;
+            list.push_back(first_checkpoint);
+            dist.push_back(next_checkpoint_dist);
+        }
 
-
-        // You have to output the target position
-        // followed by the power (0 <= thrust <= 100) or "BOOST"
-        // i.e.: "x y thrust"
         
-        int thrust = calculate_thrust(x,y, opponent_x, opponent_y, &BOOST, next_checkpoint_angle, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_prev_checkpoint_dist, 600);
+
+        int thrust = calculate_thrust(list, lap, x,y, prev_x, prev_y, next_checkpoint_angle, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_prev_checkpoint_dist, 600);
+        
+        prev_x = x;
+        prev_y=y;
+
         next_prev_checkpoint_dist = next_checkpoint_dist;
 
-        if (thrust == 101){
-            cout << next_checkpoint_x << " " << next_checkpoint_y << " " << "BOOST" << endl;
+        
+        Checkpoint nextChecpoint;
+        nextChecpoint.x = next_checkpoint_x;
+        nextChecpoint.y = next_checkpoint_y;
+        
+        Checkpoint best = CheckpointManager(list, dist, nextChecpoint, first_checkpoint, next_checkpoint_dist, lap);
+
+        if (best.x == nextChecpoint.x and nextChecpoint.y == best.y){
+            if (boost == true){ // is the boost available?
+                if (abs(next_checkpoint_angle) <= 1){
+                    cout << next_checkpoint_x << " " << next_checkpoint_y << " " << "BOOST" << endl;
+                    cerr << "BOOST" << endl;
+                    boost = false;
+                }
+                else
+                    if (thrust!=-1)
+                        cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << endl;
+            }
+            else{
+                if (thrust!=-1)
+                    cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << endl;
+            }
         }
         else{
-            cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << endl;
+            if (thrust!=-1)
+                cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << endl;
         }
+    
     }
 }
